0532701ba8dfad4046a9d522ec10704f4b25d426
Jiaxin Gu <jxgu1016@gmail.com>
2018-12-08 20:09:08 +0800

    cpu done

--- gcn/csrc/GOF.h
+++ gcn/csrc/GOF.h
@@ -8,39 +8,27 @@
 #endif
 
 // Interface for Python
-at::Tensor ROIAlign_forward(const at::Tensor& input,
-                            const at::Tensor& rois,
-                            const float spatial_scale,
-                            const int pooled_height,
-                            const int pooled_width,
-                            const int sampling_ratio) {
-  if (input.type().is_cuda()) {
+at::Tensor GOF_forward(const at::Tensor& weight, 
+                       const at::Tensor& gaborFilterBank) {
+  if (weight.type().is_cuda()) {
 #ifdef WITH_CUDA
-    return ROIAlign_forward_cuda(input, rois, spatial_scale, pooled_height, pooled_width, sampling_ratio);
+    return GOF_forward_cuda(weight, gaborFilterBank);
 #else
     AT_ERROR("Not compiled with GPU support");
 #endif
   }
-  return ROIAlign_forward_cpu(input, rois, spatial_scale, pooled_height, pooled_width, sampling_ratio);
+  return GOF_forward_cpu(weight, gaborFilterBank);
 }
 
-at::Tensor ROIAlign_backward(const at::Tensor& grad,
-                             const at::Tensor& rois,
-                             const float spatial_scale,
-                             const int pooled_height,
-                             const int pooled_width,
-                             const int batch_size,
-                             const int channels,
-                             const int height,
-                             const int width,
-                             const int sampling_ratio) {
-  if (grad.type().is_cuda()) {
+at::Tensor GOF_backward(const at::Tensor& grad_output,
+                        const at::Tensor& gaborFilterBank) {
+  if (grad_output.type().is_cuda()) {
 #ifdef WITH_CUDA
-    return ROIAlign_backward_cuda(grad, rois, spatial_scale, pooled_height, pooled_width, batch_size, channels, height, width, sampling_ratio);
+    return GOF_backward_cuda(grad_output, gaborFilterBank);
 #else
     AT_ERROR("Not compiled with GPU support");
 #endif
   }
-  AT_ERROR("Not implemented on the CPU");
+  return GOF_backward_cpu(grad_output, gaborFilterBank);
 }
 
